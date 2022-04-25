96812ef336f1e04dec777670e3ef70114eb04dcb
Josue <josuecarlosos2@gmail.com>
2022-04-25 08:17:21 -0300

    funcao estudo

--- bin/dart_colecoes.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_colecoes/dart_colecoes.dart' as dart_colecoes;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_colecoes.calculate()}!');
-}
---
+++ lib/1-funcoes/1-funcoes.dart
@@ -0,0 +1,7 @@
+void main() {
+  //funcao de nivel superior
+}
+
+int soma(int a, int b) {
+  return a + b;
+}
